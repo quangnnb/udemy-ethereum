18174484481ede83fc6e885cef1a81b87e34e667
Quang <bnnquang@gmail.com>
2023-01-03 00:02:36 +0700

    add test case

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require('../ethereum/build/CampaignFactory.json');
-const compiledCampaign = require('../ethereum/build/Campaign.json');
+const compiledFactory = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
 
 let accounts;
 let factory;
@@ -12,9 +12,31 @@ let campaignAddress;
 let campaign;
 
 beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
+  accounts = await web3.eth.getAccounts();
 
-    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface));
-        .deploy({ data: compiledFactory.bytecode})
-     .send({ from: account[0], gas: '1000000'});
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  await factory.methods.createCampaign("100").send({
+    from: accounts[0],
+    gas: "1000000",
+  });
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
+
+describe("Campaigns", () => {
+  it("deploys a factory and a campaign", () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+  it('the caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(manager, accounts[0]);
+  });
 });
