9203ee017b5e3a36bb54f153a548711e44c51d1c
Quang <bnnquang@gmail.com>
2023-01-02 22:33:32 +0700

    add test

--- compile.js
+++ compile.js
@@ -2,17 +2,17 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
-const buildPath = path.resolve(__dirname, 'build');
+const buildPath = path.resolve(__dirname, 'contracts','build');
 fs.removeSync(buildPath);
 
-const campaignPath = path.resolve(__dirname, 'Campaign.sol');
+const campaignPath = path.resolve(__dirname, 'contracts','Campaign.sol');
 const source = fs.readFileSync(campaignPath,'utf8');
 const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
 for (let contract in output) {
     fs.outputJsonSync(
-        path.resolve(buildPath, contract + '.json'),
+        path.resolve(buildPath, contract.replace(':','') + '.json'),
         output[contract]
         );
 }
