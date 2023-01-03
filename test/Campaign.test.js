87b8c10649e9a38be7c161d2a445fd857f8c0eb3
Quang <bnnquang@gmail.com>
2023-01-03 23:33:12 +0700

    add test cases

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -3,8 +3,8 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require("../ethereum/build/CampaignFactory.json");
-const compiledCampaign = require("../ethereum/build/Campaign.json");
+const compiledFactory = require("../contracts/build/CampaignFactory.json");
+const compiledCampaign = require("../contracts/build/Campaign.json");
 
 let accounts;
 let factory;
@@ -35,8 +35,62 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
   it('the caller as the campaign manager', async () => {
         const manager = await campaign.methods.manager().call();
         assert.equal(manager, accounts[0]);
+  });
+  it('allow people to contribute money and mask them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value:'200',
+      from: accounts[1]
+    });
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value:'5',
+        from: accounts[1]
+      });
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('allow a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy batteries', '100', accounts[1])
+      .send({
+        from:accounts[0],
+        gas:'1000000'
+      });
+    const request = await campaign.methods.requests(0).call();
+    
+    assert.equal('Buy batteries', request.description);  
+  });
+
+  it('processes request', async () => {
+    await campaign.methods.contribute().send({
+      from:accounts[0],
+      value: web3.utils.toWei('10','ether')
+    });
+    await campaign.methods
+      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({ from: accounts[0], gas: '1000000'});
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas:'1000000'
+    });
+    await campaign.methods.finalizeRequest(0).send({
+      from:accounts[0],
+      gas:'1000000'
+    });
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+    assert(balance > 103)
   });
 });
