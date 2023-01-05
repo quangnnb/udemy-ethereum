d49361d06b6fef6a0da08952a9796895d7728610
Quang <bnnquang@gmail.com>
2023-01-05 23:32:36 +0700

    add deploy.js

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
-const buildPath = path.resolve(__dirname, 'contracts','build');
+const buildPath = path.resolve(__dirname,'build');
 fs.removeSync(buildPath);
 
 const campaignPath = path.resolve(__dirname, 'contracts','Campaign.sol');
